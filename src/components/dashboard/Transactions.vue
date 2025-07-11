885a485d6f8bdff364bd79d6e8a51b8629ff2965
Toriola <faaedah@gmail.com>
2025-07-11 23:27:20 +0100

    UPDATE: updated transations

--- src/components/dashboard/Transactions.vue
+++ src/components/dashboard/Transactions.vue
@@ -4,21 +4,19 @@
 
       <div
          :class="`flex-items justify-between py-4 [&:not(:last-child)]:border-b border-grey-100`"
-         v-for="(item, index) in transactions"
+         v-for="(item, index) in data.transactions"
          :key="index"
       >
          <div class="flex-items gap-x-4">
-            <img
-               :src="`/assets/avatars/${item.avatar}`"
-               alt=""
-               class="h-8 w-8 rounded-full"
-            />
+            <img :src="item.avatar" alt="" class="h-8 w-8 rounded-full" />
             <p class="font-bold text-sm text-grey-900">{{ item.name }}</p>
          </div>
 
          <div>
-            <p class="text-sm font-bold text-green">{{ item.price }}</p>
-            <p class="text-xs text-grey-500 mt-2">{{ item.date }}</p>
+            <p class="text-sm font-bold text-green">{{ item.amount }}</p>
+            <p class="text-xs text-grey-500 mt-2">
+               {{ formatDate(item.date) }}
+            </p>
          </div>
       </div>
    </section>
@@ -26,37 +24,6 @@
 
 <script setup>
 import SectionTitle from './SectionTitle.vue'
-
-const transactions = [
-   {
-      avatar: 'emma-richardson.jpg',
-      name: 'Emma Richardson',
-      price: '+$75.50',
-      date: '19 Aug 2024',
-   },
-   {
-      avatar: 'savory-bites-bistro.jpg',
-      name: 'Savory Bites Bistro',
-      price: '+$55.50',
-      date: '19 Aug 2024',
-   },
-   {
-      avatar: 'daniel-carter.jpg',
-      name: 'Daniel Carter',
-      price: '+$42.30',
-      date: '18 Aug 2024',
-   },
-   {
-      avatar: 'sun-park.jpg',
-      name: 'Sun Park',
-      price: '+$120.00',
-      date: '17 Aug 2024',
-   },
-   {
-      avatar: 'urban-services-hub.jpg',
-      name: 'Urban Services Hub',
-      price: '+$65.00',
-      date: '17 Aug 2024',
-   },
-]
+import data from '../../../data.json'
+import { formatDate } from '@/utils/date'
 </script>
